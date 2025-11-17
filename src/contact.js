2386b52c29a0e10149bc2b4df8b7f82dbb91ecbc
JaneLane1704 <81980528+JaneLane1704@users.noreply.github.com>
2025-11-17 16:19:32 +0300

    add (src): upload .js files

---
+++ src/contact.js
@@ -0,0 +1,11 @@
+'use strict'
+
+export default {
+    h1: 'Аулов Илья Валерьевич',
+    about: [ '+7 920 034 46 22', 'aulov.ilya@yandex.ru'],
+    list: true,
+    next: 'Главная'
+}
+
+
+//'Контакты для связи'
